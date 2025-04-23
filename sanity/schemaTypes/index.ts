2a72a484c50a7c9d5666c2cd083c8370e7ae6121
velcrowaf <kibetonyango23@gmail.com>
2025-04-23 11:03:48 -0700

    y

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,3 +1,8 @@
-import pages from "./pages";
+import createSchema from 'part:@sanity/base/schema-creator';
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import page from './page';
 
-export const schemaTypes = [./pages]
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([page]),
+});
